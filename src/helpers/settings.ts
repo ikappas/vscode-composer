d69af8e2c76cfb1ddcf1f4d3856c9d3793378e34
Ioannis Kappas <ikappas@devworks.gr>
2017-11-08 00:49:36 +0200

    Add Settings.workingPath property

--- src/helpers/settings.ts
+++ src/helpers/settings.ts
@@ -7,34 +7,40 @@ import { workspace } from "vscode";
 import { SettingNames } from "./constants";
 
 abstract class BaseSettings {
-    protected readSetting<T>(name: string, defaultValue:T): T {
-        let configuration = workspace.getConfiguration();
-        let value = configuration.get<T>(name, undefined);
+	protected readSetting<T>(name: string, defaultValue:T): T {
+		let configuration = workspace.getConfiguration();
+		let value = configuration.get<T>(name, undefined);
 
-        // If user specified a value, use it
-        if (value !== undefined && value !== null) {
-            return value;
-        }
-        return defaultValue;
-    }
+		// If user specified a value, use it
+		if (value !== undefined && value !== null) {
+			return value;
+		}
+		return defaultValue;
+	}
 }
 
 export class Settings extends BaseSettings {
-    private _enabled: boolean;
-    private _executablePath: string;
+	private _enabled: boolean;
+	private _executablePath: string;
+	private _workingPath: string;
 
-    constructor() {
-        super();
+	constructor() {
+		super();
 
 		this._enabled = this.readSetting<boolean>(SettingNames.Enabled, true);
-        this._executablePath = this.readSetting<string>(SettingNames.ExecutablePath, undefined);
-    }
+		this._executablePath = this.readSetting<string>(SettingNames.ExecutablePath, undefined);
+		this._workingPath = this.readSetting<string>(SettingNames.WorkingPath, undefined);
+	}
 
 	public get enabled(): boolean {
 		return this._enabled;
 	}
 
-    public get executablePath(): string {
-        return this._executablePath;
-    }
+	public get executablePath(): string {
+		return this._executablePath;
+	}
+
+	public get workingPath(): string {
+		return this._workingPath;
+	}
 }
