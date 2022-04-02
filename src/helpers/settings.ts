694829267f2e925ae4018f35a3983eb8059eb9e1
Ioannis Kappas <ioannis@kappas.io>
2022-04-03 00:00:41 +0300

    Cleanup ComposerSettings class

--- src/helpers/settings.ts
+++ src/helpers/settings.ts
@@ -5,7 +5,7 @@
 'use strict';
 
 import { Constants } from "./constants";
-import { workspace, Uri, WorkspaceConfiguration } from "vscode";
+import { ConfigurationScope, workspace, WorkspaceConfiguration } from "vscode";
 
 export class SettingNames {
 	static SettingsPrefix: string = Constants.ExtensionName + '.';
@@ -14,79 +14,23 @@ export class SettingNames {
 	static WorkingPath: string = SettingNames.SettingsPrefix + 'workingPath';
 }
 
-abstract class BaseSettings {
+export class ComposerSettings {
+	private _config: WorkspaceConfiguration
 
-	/**
-	 * The resource for which the settings apply.
-	 */
-	private _resource: Uri;
-
-	/**
-	 * Class constructor.
-	 * @param resource The resource for which the settings apply.
-	 */
-	constructor(resource: Uri = undefined) {
-		this._resource = resource;
-	}
-
-	/**
-	 * Get the resource for which the settings apply.
-	 */
-	public get resource(): Uri {
-		return this._resource;
-	}
-
-	/**
-	 * Read the setting.
-	 *
-	 * @param name The name of the setting.
-	 * @param defaultValue The default value for the setting.
-	 */
-	protected readSetting<T>(name: string, defaultValue: T): T {
-		let config: WorkspaceConfiguration;
-
-		if (this.resource === null || this.resource === undefined) {
-			// Reading Window scoped configuration
-			config = workspace.getConfiguration('', null);
-		} else {
-			// Reading Resource scoped configuration
-			config = workspace.getConfiguration('', this.resource);
-		}
-
-		const value = config.get<T>(name, undefined);
-
-		// If user specified a value, use it
-		if (value !== undefined && value !== null) {
-			return value;
-		}
-
-		return defaultValue;
-	}
-}
-
-export class ComposerSettings extends BaseSettings {
-	private _enabled: boolean;
-	private _executablePath: string;
-	private _workingPath: string;
-
-	constructor(resource: Uri = null) {
-		super(resource);
-
-		this._enabled = this.readSetting<boolean>(SettingNames.Enabled, true);
-		this._executablePath = this.readSetting<string>(SettingNames.ExecutablePath, undefined);
-		this._workingPath = this.readSetting<string>(SettingNames.WorkingPath, undefined);
+	constructor(scope?: ConfigurationScope) {
+		this._config = workspace.getConfiguration('', scope);
 	}
 
 	public get enabled(): boolean {
-		return this._enabled;
+		return this._config.get<boolean>(SettingNames.Enabled, true);
 	}
 
 	public get executablePath(): string {
-		return this._executablePath;
+		return this._config.get<string>(SettingNames.ExecutablePath, undefined);
 	}
 
 	public get workingPath(): string {
-		return this._workingPath;
+		return this._config.get<string>(SettingNames.WorkingPath, undefined);
 	}
 }
 
